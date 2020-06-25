b0806e3a186d0ded207886c052bc5a1c28403d62
Rafael Martins <rafael.martins92@outlook.com>
2020-06-25 20:43:49 -0300

    Adjusting Initial Screen

--- scripts/screens/initialScreen.js
+++ scripts/screens/initialScreen.js
@@ -15,11 +15,19 @@ class InitialScreen {
 
   _text() {
     textFont(fontInitialScreen);
-    textAlign('center');  
-    textSize(50);
-    text('As aventuras de', width/2, height/3);
+    textAlign('right');  
+    fill(74, 76, 105);
+    textSize(70);
+    text('As aventuras de', width/2, height/10 * 4);
+    textAlign('left');  
     textSize(150);
-    text('Chubby', width/2, height/5 * 3)
+    text(' Chubby', width/2, height/10 * 4.4);
+    textAlign('center');
+    fill(90, 92, 120);
+    textSize(40);
+    text('Chubby se perdeu no meio da floresta!!!', width/2, height/10 * 5.1)
+    text('Dizem que pela noite', width/2, height/10 * 5.6)
+    text('monstros costumam vagar por essa floresta...', width/2, height/10 * 6.1)
   };
 
   _button() {
